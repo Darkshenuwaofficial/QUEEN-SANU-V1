60c0b236ef754c9fe33082cea3b82aea442af2ad
Darkshenuwa <darkshenuwaid@gmail.com>
2024-05-18 20:48:22 +0530

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,1 +1,22 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
 
+
+zokou({ nomCom: 'ping',
+    desc: 'To check ping',
+    Categorie: 'General',
+    reaction: '🧞‍♀️', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start} = new Date().getTime()
+    return repondre('📌 *QUEEN SANU-MD-V1 🍁 PING*\n ```' + 2000 + '``` *ms*') 
+    const { end } = new Date().getTime()
+    await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
+  }
+)
